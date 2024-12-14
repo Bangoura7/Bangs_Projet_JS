b5a419cae0fa894b0ab04574000731b01ae1d12a
Bangoura7 <bangsabdallah4@gmail.com>
2024-12-14 15:44:16 +0000

    l'ajout de la fonction rangeDeligne pour le nombre de ligne de la pyramide et le numero de ligne de la pyramide

--- Génerateur pyramidal/main.js
+++ Génerateur pyramidal/main.js
@@ -1,18 +1,30 @@
-let caracter = "*";
-let comptage = 8;
-let lignes = [];
-let resultat = ""
+let caracter = "*"
+let comptage = 8
+let lignes = []
 
-for (let i = 0; i < comptage; i++) {
 
-  lignes.push(caracter.repeat(i));  
+
+function rangeDeLigne(numeroDeLigne, nombreDeLigne) {
+  return ( 
+   " ".repeat(nombreDeLigne - numeroDeLigne) +
+   caracter.repeat(numeroDeLigne) +
+   " ".repeat(nombreDeLigne - numeroDeLigne)
+  )
 
 }
 
 
+for (let i = 0; i < comptage; i++) {
+
+  lignes.push(rangeDeLigne(i + 1, comptage));  
+
+}
+
+let resultat = ""
 for (const ligne of lignes) {
     resultat += ligne + "\n"
 }
 console.log(resultat)
+/* les fonctions */
 
 
