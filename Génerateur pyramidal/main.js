84d9aa7548f20403d3068de26d3d2c919fb7e23d
Bangoura7 <bangsabdallah4@gmail.com>
2024-12-16 14:19:32 +0000

    finalisation du paramyde

--- Génerateur pyramidal/main.js
+++ Génerateur pyramidal/main.js
@@ -1,24 +1,40 @@
 let caracter = "*"
 let comptage = 8
 let lignes = []
-
+let inverse = false
 
 
 function rangeDeLigne(numeroDeLigne, nombreDeLigne) {
   return ( 
    " ".repeat(nombreDeLigne - numeroDeLigne) +
-   caracter.repeat(numeroDeLigne) +
+   caracter.repeat(2 * numeroDeLigne -1) +
    " ".repeat(nombreDeLigne - numeroDeLigne)
   )
 
 }
+/* Les differents type de boucle pour faire le pyramide */
+
+for (let i = 1; i < comptage; i++) {
+
+    if (inverse) {
+      lignes.unshift(rangeDeLigne(i  , comptage));
+    }else {
+      lignes.push(rangeDeLigne(i  , comptage));
+    }
+    
+
+} 
+
+/* while (lignes.length < comptage) {
+  lignes.push(rangeDeLigne(lignes.length + 1, comptage))
+  
+} */
+
+/* for (let i = comptage; i > 0; i--) {
+  lignes.push(rangeDeLigne(i, comptage))
+} */
 
 
-for (let i = 0; i < comptage; i++) {
-
-  lignes.push(rangeDeLigne(i + 1, comptage));  
-
-}
 
 let resultat = ""
 for (const ligne of lignes) {
