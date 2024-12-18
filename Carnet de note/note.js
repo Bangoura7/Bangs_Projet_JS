5c56ca352691ae8c9c1285ab013ffd7fc157581d
Bangoura7 <bangsabdallah4@gmail.com>
2024-12-18 12:15:44 +0000

    l'ajout d'un nouveau projet jeu de rôle

--- Carnet de note/note.js
+++ Carnet de note/note.js
@@ -32,6 +32,35 @@ console.log(noteMention(71))
 console.log(noteMention(65))
 console.log(noteMention(30))
 
+function admissionEleve(notes) {
+
+  if (noteMention(notes) !== "F") {
+    return "Vous êtes admis avec une note de : " + notes
+  }else {
+    return "Désolez vous êtes pas admis avec une note : " + notes
+  }
+}
+console.log(admissionEleve(100))
+console.log(admissionEleve(85))
+console.log(admissionEleve(98))
+console.log(admissionEleve(71))
+console.log(admissionEleve(65))
+console.log(admissionEleve(30))
+
+
+function envoiMsg(noteClasse, noteEleve) {
+  const moyenneClasse = moyenneEleve(noteClasse)
+  const moyenEleve =  noteMention(noteEleve)
+
+  if (admissionEleve(noteEleve)) {
+    return ("Moyenne de la classe : " + moyenneClasse + ".  Votre note : " + moyenEleve + ". Vous avez échouer au cours.");
+  }else {
+    return ("Moyenne de la classe : " + moyenneClasse + ".  Votre note : " + moyenEleve + ". Vous avez reussir le cours.");
+  }
+}
+
 /* Moyenne de la classe : average-goes-here. Votre note : note- va ici. Vous avez échoué au cours. */
 
 /* Moyenne de la classe : average-goes-here. Votre note : grade-goes-here. Vous avez réussi le cours. */
+
+console.log(envoiMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37))
