547e5a2f38a5f4b1e08124254d27f3f6b1178e12
Bangoura7 <bangsabdallah4@gmail.com>
2024-12-18 15:04:43 +0000

    La declaration des variable et la conception du body de la page

---
+++ jeu de role/main.js
@@ -0,0 +1,11 @@
+let xp = 0;
+let vie = 100;
+let or = 50;
+let indexDeLarmeActuel = 0;
+let combat ;
+let vieDuMonstre;
+let inventaire = ["stick"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
