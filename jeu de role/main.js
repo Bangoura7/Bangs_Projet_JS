073dcab001468a8ee93ffe3981dfaf80a2dc3b51
Bangoura7 <bangsabdallah4@gmail.com>
2024-12-19 11:50:24 +0000

    la mise forme de page

--- jeu de role/main.js
+++ jeu de role/main.js
@@ -6,6 +6,25 @@ let combat ;
 let vieDuMonstre;
 let inventaire = ["stick"];
 
+
+
+const xpTexte = document.querySelector("#xpTexte");
+const vieTexte= document.querySelector("#vieTexte");
+const OrTexte= document.querySelector("#OrTexte");
+
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+
+const  nomMonstre = document.querySelector("#nomMonstre");
+const  vieMonstre= document.querySelector("#vieMonstre");
+
+const texte = document.querySelector("#texte");
+
+const vieDuMonstreText = document.querySelector("#vieDuMonstre");
+
+
+
+function allerAuMagasin() {
+
+}
